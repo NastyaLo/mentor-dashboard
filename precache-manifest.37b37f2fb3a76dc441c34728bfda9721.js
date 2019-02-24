49a3b061a87b01db342374443a2fd0cb12110d84
NastyaLo <loyko2802@gmail.com>
2019-02-24 14:56:05 +0300

    deploy mentor-dashboard

---
+++ precache-manifest.37b37f2fb3a76dc441c34728bfda9721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5357f7305c488abea30c",
+    "url": "/mentor-dashboard/static/js/runtime~main.5357f730.js"
+  },
+  {
+    "revision": "756f59acb773b35ec103",
+    "url": "/mentor-dashboard/static/js/main.756f59ac.chunk.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/mentor-dashboard/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "756f59acb773b35ec103",
+    "url": "/mentor-dashboard/static/css/main.e7ea18cd.chunk.css"
+  },
+  {
+    "revision": "991ac4959a82f34a45723acc92d971bb",
+    "url": "/mentor-dashboard/index.html"
+  }
+];
